8cb73ddd6f2b9699378764dc4b5b72aeadd084eb
davidbi <davidbi@nice.com>
2021-08-15 17:03:39 +0300

    E2E pass with assert

--- src/test/java/pages/Actions.java
+++ src/test/java/pages/Actions.java
@@ -17,42 +17,58 @@ public class Actions {
 
     private static WebDriver driver;
 
+    /**
+     * click on any Element
+     * @param locator - By.Id/cssLocator etc...
+     */
     public void clickElement(By locator) {
 
         getWebElement(locator).click();
     }
 
+    /**
+     * send keys or string to any Element
+     * @param locator - By.Id/cssLocator etc...
+     * @param text - the keys you want to send
+     */
     public void sendKeysToElement(By locator, String text) {
         getWebElement(locator).sendKeys(text);
     }
 
+    /**
+     *
+     * @param locator - By.Id/cssLocator etc...
+     * @return - returns the value of the text value in text boxes
+     */
     public String getElementAttribute(By locator) {
         return getWebElement(locator).getAttribute("value");
     }
 
+    /**
+     *
+     * @param locator By.Id/cssLocator etc...
+     * @return driver object instance for the WebElement
+     */
     private WebElement getWebElement(By locator) {
         return DriverSingleton.getDriverInstance().findElement(locator);
     }
 
-    public void DropDownPicker(By locator,By locatorValue) {
-        getWebElement(locator).click();
-        getWebElement(locatorValue).click();
-    }
 
+    /**
+     * clearing text boxes values
+     * @param locator - By.Id/cssLocator etc...
+     */
     public void clearTextBox(By locator) {
         getWebElement(locator).clear();
     }
 
+    /**
+     * explicit wait for element to be visible
+     * @param locator - By.Id/cssLocator etc...
+     */
     public void waitForElementToAppear(By locator) {
         waitForElement().until(elementToBeClickable(locator));
     }
-
-    public void listElementsClickOnIndex(By locator,int index) {
-        waitForElement().until(elementToBeClickable(locator));
-        List<WebElement> elementsList = driver.findElements(locator);
-        elementsList.get(index).click();
-    }
-
 
 
 }
